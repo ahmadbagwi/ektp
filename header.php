a9fd2521174431f43172a05b693c22e911efdfb0
ahmadbagwi <ahmad@kdesain.com>
2018-02-28 16:12:16 +0700

    edit header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@ $path = $_SERVER['DOCUMENT_ROOT'];
 include($_SERVER['DOCUMENT_ROOT'].'/php/ektp/db.php');
 $user = $_COOKIE['user'];
 $pass = $_COOKIE['pass'];
-ini_set('display_errors', 1);
+
 if (!isset($user)) {
 	echo "<script>alert('Anda belum login, silahkan login terlebih dahulu ya');document.location='../';</script>";
 }
